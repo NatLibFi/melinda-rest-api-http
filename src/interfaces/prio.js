c0a4e5f5dca6f061df3d9d62a8be1b144da16710
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2020-11-03 08:24:59 +0200

    remove done queue-items simpler

--- src/interfaces/prio.js
+++ src/interfaces/prio.js
@@ -91,16 +91,13 @@ export default async function ({sruUrl, amqpUrl, mongoUri, pollWaitTime}) {
     await amqpOperator.ackMessages([message]);
     await amqpOperator.removeQueue(correlationId);
 
-    if (!noop) { // eslint-disable-line functional/no-conditional-statement
-      await mongoOperator.remove(correlationId);
-    }
-
     if (responseData.status === 'CREATED') {
-      await mongoOperator.pushId({correlationId, id: responseData.payload || undefined});
       // Reply to http
       if (noop) {
         return responseData.messages;
       }
+
+      await mongoOperator.remove(correlationId);
       return {messages: responseData.messages, id: responseData.payload};
     }
 
@@ -140,16 +137,13 @@ export default async function ({sruUrl, amqpUrl, mongoUri, pollWaitTime}) {
     await amqpOperator.ackMessages([message]);
     await amqpOperator.removeQueue(correlationId);
 
-    if (!noop) { // eslint-disable-line functional/no-conditional-statement
-      await mongoOperator.remove(correlationId);
-    }
-
     if (responseData.status === 'UPDATED') {
-      await mongoOperator.pushId({correlationId, id: responseData.payload || undefined});
       // Reply to http
       if (noop) {
         return responseData.messages;
       }
+
+      await mongoOperator.remove(correlationId);
       return responseData;
     }
 
