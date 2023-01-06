9bfca5a867e86da840b3a936bb80ff6e94271e46
Minttu Hurme <ammsalme@users.noreply.github.com>
2023-01-06 13:41:54 +0200

    Release v3 (#75)
    * From Node 14 to Node 18
    * From Drone to GitHub Actions
    * Add Openshift web hooks
    
    * Update responses
    * Add Batch bulk -functionality
    * Add Merge-functionality
    * Add Logs-functionality
    * Add different handling for different record types (bib/aut)
    
    Co-authored-by: Joni Ollila <53558796+natlibfi-jonollil@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ src/interfaces/utils.js
@@ -0,0 +1,102 @@
+import moment from 'moment';
+import sanitize from 'mongo-sanitize';
+import {createLogger} from '@natlibfi/melinda-backend-commons';
+import {parseBoolean} from '@natlibfi/melinda-commons';
+
+const logger = createLogger();
+
+// Query filters
+// skip int 0-999999 skips from results
+// limit int 0-999999 limits response amount
+// id uuid v4 correlationId
+// queueItemState string enum QUEUE_ITEM_STATE
+// creationTime stringified array of utc timestamps 1 specific or 2 for range
+// modificationTime stringified array of utc timestamps 1 specific or 2 for range
+
+// eslint-disable-next-line max-statements
+export function generateQuery({id, correlationId, queueItemState, creationTime, modificationTime, skip, limit}) {
+  const doc = {};
+
+  if (skip) { // eslint-disable-line functional/no-conditional-statement
+    doc.skip = skip; // eslint-disable-line functional/immutable-data
+  }
+
+  if (limit) { // eslint-disable-line functional/no-conditional-statement
+    doc.limit = limit; // eslint-disable-line functional/immutable-data
+  }
+
+  // We parse both 'id' and 'correlationId' in query as correlationId in Mongo
+
+  if (id) { // eslint-disable-line functional/no-conditional-statement
+    doc.correlationId = sanitize(id); // eslint-disable-line functional/immutable-data
+  }
+
+  if (correlationId) { // eslint-disable-line functional/no-conditional-statement
+    doc.correlationId = sanitize(correlationId); // eslint-disable-line functional/immutable-data
+  }
+
+  // we could have here also final: ABORT, DONE, ERROR, active: !final
+  if (queueItemState) { // eslint-disable-line functional/no-conditional-statement
+    doc.queueItemState = queueItemState; // eslint-disable-line functional/immutable-data
+  }
+
+  if (creationTime) {
+    const timestampArray = JSON.parse(creationTime);
+    if (creationTime.length === 1) { // eslint-disable-line functional/no-conditional-statement
+      doc.creationTime = formatTime(timestampArray[0]); // eslint-disable-line functional/immutable-data
+    } else { // eslint-disable-line functional/no-conditional-statement
+      doc.$and = [ // eslint-disable-line functional/immutable-data
+        {creationTime: {$gte: formatTime(timestampArray[0])}},
+        {creationTime: {$lte: formatTime(timestampArray[1])}}
+      ];
+    }
+  }
+
+  if (modificationTime) {
+    const timestampArray = JSON.parse(modificationTime);
+    if (modificationTime.length === 1) { // eslint-disable-line functional/no-conditional-statement
+      doc.modificationTime = formatTime(timestampArray[0]); // eslint-disable-line functional/immutable-data
+    } else { // eslint-disable-line functional/no-conditional-statement
+      doc.$and = [ // eslint-disable-line functional/immutable-data
+        {modificationTime: {$gte: formatTime(timestampArray[0])}},
+        {modificationTime: {$lte: formatTime(timestampArray[1])}}
+      ];
+    }
+  }
+
+  return doc;
+
+  function formatTime(timestamp) {
+    logger.debug(`Timestamp: ${timestamp}`);
+    // Ditch the timezone
+    const time = moment.utc(timestamp);
+    logger.silly(time);
+    return time.toDate();
+  }
+}
+
+export function generateShowParams({showAll = 0, showOperations = undefined, showOperationSettings = undefined, showRecordLoadParams = undefined, showImportJobState = undefined}) {
+  if (parseBoolean(showAll)) {
+    return {_id: 0};
+  }
+
+  const paramsArray = [
+    {showOperations: showOperations ? showOperations : false},
+    {showOperationSettings: showOperationSettings ? showOperationSettings : false},
+    {showRecordLoadParams: showRecordLoadParams ? showRecordLoadParams : false},
+    {showImportJobState: showImportJobState ? showImportJobState : false}
+  ].filter(param => param);
+
+  return Object.assign({_id: 0}, ...paramsArray);
+}
+
+
+/*
+export function generateShowParams({showAll = 0, showOperations = 0, showOperationSettings = 0, showRecordLoadParams = 0, showImportJobState = 0}) {
+  if (parseBoolean(showAll)) {
+    return {showOperations: 1, showOperationSettings: 1, showRecordLoadParams: 1, showImportJobState: 1};
+  }
+
+  return {showOperations, showOperationSettings, showRecordLoadParams, showImportJobState};
+}
+*/
---
+++ src/routes/logs.js
@@ -0,0 +1,107 @@
+import {Router} from 'express';
+import passport from 'passport';
+import {createLogger} from '@natlibfi/melinda-backend-commons';
+import {Error as HttpError, parseBoolean} from '@natlibfi/melinda-commons';
+import {checkQueryParams} from './queryUtils';
+import {authorizeKVPOnly, checkId} from './routeUtils';
+import createService from '../interfaces/logs';
+
+export default async function ({mongoUri}) {
+  const logger = createLogger();
+
+  const Service = await createService({mongoUri});
+
+  return new Router()
+    .use(passport.authenticate('melinda', {session: false}))
+    .use(authorizeKVPOnly)
+    .use(checkQueryParams)
+    .get('/list', getListOfLogs)
+    .get('/:id', checkId, getLogs)
+    .get('/', doLogsQuery)
+    .put('/:id', checkId, protectLog)
+    .delete('/:id', checkId, removeLog);
+
+  async function doLogsQuery(req, res, next) {
+    logger.verbose('routes/logs doLogsQuery');
+    try {
+      logger.debug(JSON.stringify(req.query));
+      const response = await Service.doLogsQuery(req.query);
+      res.json(response);
+    } catch (error) {
+      if (error instanceof HttpError) {
+        res.status(error.status).send(error.payload);
+        return;
+      }
+      return next(error);
+    }
+  }
+
+  async function getLogs(req, res, next) {
+    logger.verbose('routes/logs getLogs');
+    try {
+      logger.debug(`We have a correlationId: ${req.params.id}`);
+      const response = await Service.getLogs({correlationId: req.params.id});
+      res.status(response.status).json(response.payload);
+    } catch (error) {
+      if (error instanceof HttpError) {
+        return res.status(error.status).send(error.payload);
+      }
+      return next(error);
+    }
+  }
+
+  async function getListOfLogs(req, res, next) {
+    logger.verbose('routes/logs getListOfLogs');
+    try {
+      const expanded = req.query === undefined || req.query.expanded === undefined ? false : parseBoolean(req.query.expanded);
+      const {logItemType} = req.query === undefined || req.query.logItemType === undefined ? 'MERGE_LOG' : req.query.logItemType;
+      if (expanded !== true) {
+        logger.debug(`Getting list of logs ${logItemType}`);
+        const response = await Service.getListOfLogs(logItemType);
+        res.status(response.status).json(response.payload);
+        return;
+      }
+      logger.debug(`Getting expanded list of logs`);
+      const response = await Service.getExpandedListOfLogs();
+      res.status(response.status).json(response.payload);
+      return;
+    } catch (error) {
+      if (error instanceof HttpError) {
+        return res.status(error.status).send(error.payload);
+      }
+      return next(error);
+    }
+  }
+
+  async function protectLog(req, res, next) {
+    logger.verbose('routes/logs protectLog');
+    try {
+      const correlationId = req.params.id;
+      const {blobSequence} = req.query || false;
+      logger.debug(`We have a correlationId: ${correlationId}${blobSequence ? `, blobSequence: ${blobSequence}` : ''}`);
+      const response = await Service.protectLog(correlationId, blobSequence);
+      res.status(response.status).json(response.payload);
+    } catch (error) {
+      if (error instanceof HttpError) {
+        return res.status(error.status).send(error.payload);
+      }
+      return next(error);
+    }
+  }
+
+  async function removeLog(req, res, next) {
+    logger.verbose('routes/logs removeLog');
+    try {
+      const correlationId = req.params.id;
+      const {force} = req.query || 0;
+      logger.debug(`We have a correlationId: ${correlationId}`);
+      const response = await Service.removeLog(correlationId, parseBoolean(force));
+      res.status(response.status).json(response.payload);
+    } catch (error) {
+      if (error instanceof HttpError) {
+        return res.status(error.status).send(error.payload);
+      }
+      return next(error);
+    }
+  }
+}
